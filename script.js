9834d153e3e749d708fe6976897ba7efe80e55f1
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-02-12 18:19:29 -0300

    Revealing elements on scroll

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const introBtn = document.querySelector(".intro-box-btn");
 const earlyBox = document.querySelector(".early-box");
 const inputValue = document.querySelector("input");
 const earlyBoxBtn = document.querySelector(".early-box-btn");
+const allSections = document.querySelectorAll(".section");
 
 introBtn.addEventListener("click", (event) => {
   event.preventDefault();
@@ -27,4 +28,24 @@ earlyBoxBtn.addEventListener("click", (event) => {
     earlyBox.append(errorMessage);
     setInterval(() => errorMessage.remove(), 2700);
   }
+});
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) {
+    return;
+  } else {
+    entry.target.classList.remove("section--hidden");
+    observer.unobserve(entry.target);
+  }
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.7,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
 });
